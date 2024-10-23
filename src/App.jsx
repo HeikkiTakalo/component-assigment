afb8a07164865e8423306ab927f7cfd2a12a387f
HeikkiTakalo <heikkitakalo@windowslive.com>
2024-10-23 18:35:11 +0300

    added header to app.jsx and some styling

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Header from './header';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <Header title="Welcome to product page!" />
+    </div>
+  );
+};
+
+
+export default App;
